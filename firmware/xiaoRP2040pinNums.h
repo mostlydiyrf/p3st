36558146b39f790efb293bbc85a55816e050f765
mostlydiyrf <103088630+mostlydiyrf@users.noreply.github.com>
2024-02-03 14:16:07 -0800

    Add files via upload
    Pin-number aliases for use with RP2040 boards.

---
+++ firmware/xiaoRP2040pinNums.h
@@ -0,0 +1,41 @@
+// Header file to create aliases for the actual RP2040 chip pin numbers to
+// pin numbers printed on the Xiao RP2040 module. Include this file in a sketch
+// to use "D0," "D1," etc. The "D" is not case sensitive.
+// 
+// There is no corresponding CPP file.
+//
+// Todd F. Carney 2024 Public domain
+
+
+// Digital pins
+#define D0 26u
+#define d0 26u
+#define D1 27u
+#define d1 27u
+#define D2 28u
+#define d2 28u 
+#define D3 29u
+#define d3 29u 
+#define D4 6u
+#define d4 6u 
+#define D5 7u
+#define d5 7u
+#define D6 0u
+#define d6 0u 
+#define D7 1u
+#define d7 1u
+#define D8 2u
+#define d8 2u
+#define D9 4u
+#define d9 4u
+#define D10 3u
+#define d10 3u
+// Analog pins
+#define A0 26u
+#define a0 26u
+#define A1 27u
+#define a1 27u
+#define A2 28u
+#define a2 28u
+#define A3 29u
+#define a3 29u
